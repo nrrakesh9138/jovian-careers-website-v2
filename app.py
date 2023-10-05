97e683c0d28010615edc70f881ba3e02a7f812b5
nrrakesh9138 <143411578+nrrakesh9138@users.noreply.github.com>
2023-10-05 18:58:51 +0530

    Job item commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 import json
 app = Flask(__name__)
 
@@ -13,10 +13,18 @@ def hello_world():
     jobs = load_jobs_from_db()
     return render_template('home.html', jobs=jobs, company="Jovian")
 
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not found", 404
+    else:
+        return render_template('jobpage.html', job=job)
+
 @app.route("/api/jobs")
 def list_jobs():
     jobs = load_jobs_from_db()
-    return  json.dumps(jobs, default=str)
+    return json.dumps(jobs, default=str)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -19,3 +19,16 @@ def load_jobs_from_db():
         for row in result_all:
             jobs.append(row._mapping)
         return jobs
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text(f"SELECT * FROM jobs WHERE id = {id}")
+        )
+        rows = result.all()
+        if len(rows) ==0:
+            return None
+        else:
+            return dict(rows[0]._mapping)
+
+# if __name__=="__main__":
+#     print(load_job_from_db(1))
