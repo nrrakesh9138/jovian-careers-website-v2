e395628f911829e47126b0a1ef6f589586c5bfca
nrrakesh9138 <143411578+nrrakesh9138@users.noreply.github.com>
2023-10-12 11:56:57 +0530

    add api for job id

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ def list_jobs():
     jobs = load_jobs_from_db()
     return json.dumps(jobs, default=str)
 
+@app.route("/api/job/<id>")
+def show_job_json(id):
+    job = load_job_from_db(id)
+    return jsonify(job)
+
 @app.route("/job/<id>/apply", methods = ['POST'])
 def apply_to_job(id):
     job = load_job_from_db(id)
