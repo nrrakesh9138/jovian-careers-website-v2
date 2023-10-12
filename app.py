47e0c7d8af1869b8ceb13360d9e79acafb7eb994
nrrakesh9138 <143411578+nrrakesh9138@users.noreply.github.com>
2023-10-12 10:49:21 +0530

    Dynamic inserting of application data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 import json
 app = Flask(__name__)
 
@@ -25,6 +25,16 @@ def show_job(id):
 def list_jobs():
     jobs = load_jobs_from_db()
     return json.dumps(jobs, default=str)
+
+@app.route("/job/<id>/apply", methods = ['POST'])
+def apply_to_job(id):
+    job = load_job_from_db(id)
+    data = request.form
+    add_application_to_db(id, data)
+    return render_template('application_submitted.html', application = data, job=job)
+
+
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -30,5 +30,12 @@ def load_job_from_db(id):
         else:
             return dict(rows[0]._mapping)
 
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        conn.execute(query, {"job_id":job_id, "full_name":data['full_name'], "email":data['email'], "linkedin_url":data['linkedin_url'], "education":data['education'], "work_experience":data['work_experience'], "resume_url":data['resume_url'] })
+        
+                     
+
 # if __name__=="__main__":
-#     print(load_job_from_db(1))
+#     print(add_application_to_db(1))
